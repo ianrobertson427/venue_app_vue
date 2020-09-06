9e9a43fea6b319908085a2c83e97f4e46a4f152d
“irobetson” <ianrobertson427@gmail.com>
2020-09-06 15:32:25 -0500

    Adding button and moving search bar

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,8 +1,19 @@
 <template>
   <div class="home">
+    <p> hello hello hello</p>
+    <h1>Testing Venues</h1>
+    <div class="wrapper">
+      <input v-model="search_term" type="text" class="input" placeholder="What Events would you like?">
+      <div class="searchbtn">
+        <i class="fas fa-search"></i>
+
+      </div>
+      
+      <button type="button"v-on:click="getevents()" >Click me!</button>
+
+    </div>
     <h1>{{ message }}</h1>
     <h1>{{ events }}</h1>
-    <p> hello hello hello</p>
   </div>
 </template>
 <style>
@@ -14,6 +25,7 @@ export default {
     return {
       message: "Welcome to Vue.js!",
       events: [],
+      search_term: "",
     };
   },
   created: function () {
@@ -25,7 +37,7 @@ export default {
         "Bearer UDXYtLXMGfqV_Y0ozyO5UwmE8KukBsNgwAh5hyTy";
       axios
         .get(
-          "https://api.predicthq.com/v1/events/?active.gte=2015-04-01&active.lte=2020-04-30&category=concerts&sort=rank&within=100mi@34.0522,-118.2437"
+          "https://api.predicthq.com/v1/events/?active.gte=2015-04-01&active.lte=2020-04-30&category=concerts&sort=rank&within=500mi@51.5074,0.1278"
         )
         .then((response) => {
           console.log(response.data);
